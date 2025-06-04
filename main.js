567837e2522798d3d94ef18a3ada86b9f4609b26
Loris <Loris@gmail.com>
2025-06-04 09:40:41 +0200

    aggiunto tutte le caratteristiche degli animali, tranne per l'eta che rimane un piccolo problema

--- main.js
+++ main.js
@@ -15,27 +15,54 @@ async function displayPets() {
 
     console.log(template)
 
+    const today = new Date();
+    let year = today.getFullYear()
+
     Pets.forEach(pet=>{
         const clone = template.content.cloneNode
         (true)
 
         // qui modifichiamo il template
 
-        //aggiorniamo le foto
+        //aggiorniamo le foto 
         const image = clone.querySelector('.animal-card-photo img')
         image.src = pet.photo
-        console.log(pet.photo)
+        //console.log(pet.photo)
         //aggiorniamo i nomi
-        const name = clone.querySelector('h1')
-        name.text = pet.name
-        console.log(pet.name)
+        const name = clone.querySelector('.animal-name')
+        name.textContent= pet.name
+        //console.log(pet.name)
         //aggiorniamo le speci
-        const species = clone.querySelector('span')
-        name.h1 = pet.name
-        console.log(pet.name)
+        const species = clone.querySelector('.second-span-species')
+        species.textContent = pet.species 
+        console.log(pet.species)
         //aggiorniamo le età
+        const birthYear = clone.querySelector('.first-span-age')
+        
+        // console.log(pet.birthYear)
+
+        let age = year-pet.birthYear
+        // console.log(age)
+        birthYear.textContent = age 
+        
+        if (age < 1){
+            age = " is less than one year old";
+        } else if (age == 1){
+            age ="is one year old";
+        } else  (age > 1){
+            age = "is  years old";
+        }
+        // problema rimane qua, non mi carica l'eta e mi rende il sito vuoto
+        
+
+
         //aggiorniamo le descrizioni
-        // aggiungiamo l'articolo alla pagina
+        const description = clone.querySelector('.animal-description')
+        description.textContent = pet.description 
+        // console.log(pet.description)
+        
+        
+
         wrapper.appendChild(clone)
 
         }
